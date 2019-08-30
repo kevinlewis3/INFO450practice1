819d751d497cbe7633a618e261fcf75203e12fca
Kevin Lewis <lewisks3@vcu.edu>
2019-08-29 20:44:40 -0400

    1st Commit
    1st commit = lewisks3

---
+++ cmake-build-debug/CMakeFiles/INFO450practice1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/INFO450practice1.dir/main.cpp.o"
+  "INFO450practice1.pdb"
+  "INFO450practice1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/INFO450practice1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/INFO450practice1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for INFO450practice1.
+# This may be replaced when dependencies are built.
