a2c55f4f7cb8a19b5df4acfa4e3c8cd336a6375a
Kevin Lewis <lewisks3@vcu.edu>
2019-08-29 21:02:38 -0400

    Modifications
    Completed the assignment. Everything is working.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
+
 
 int main()
 {
-    
+    int value = 1;
+    while (value != 0)
+    {
+        cout << "Please enter a value" << endl;
+        cin >> value;
+        if(value == 0)
+            break;
+        cout << "The square of your value is " << pow(value,2) <<endl;
+    }
 
     return 0;
 }
